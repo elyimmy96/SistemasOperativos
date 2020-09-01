3bd23b67903efaae43e24d61a31b8f96675fd081
Simon Green <elyimmy96@gmail.com>
2020-09-01 16:22:59 -0500

    Filosofos v1.0

---
+++ filosofos.c
@@ -0,0 +1,39 @@
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+int numFilosofos = 5;
+int tenedores[numFilosofos];
+sem_t sem;
+
+void* comer(void *param);
+
+int main(int argc, char *argv[]) {
+  pthread_t threads[numFilosofos];
+  pthread_attr_t attr;
+  int par[numFilosofos];
+  pthread_attr_init(&attr);
+  for(int i = 0; i < numFilosofos; i++) {
+    pthread_create(&threads[i], &attr, sorter, i);
+  }
+  sem_init(&sem, 0, 4);
+
+  for (int i = 0; i < numFilosofos; i++) {
+      pthread_join(threads[i], NULL);
+  }
+
+  sem_destroy(&sem);
+
+}
+void* comer(void *param) {
+  int num = (int) param;
+  printf("Filosofo %d intentando comer.\n", num);
+  sem_wait(&sem);
+  printf("Filosofo %d comiendo.\n", num);
+  sleep(2);
+  printf("Filosofo %d termino de comer.\n", num);
+  sem_post(&sem);
+  pthread_exit(0);
+}
