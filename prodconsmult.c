0dba5385d045c3dfdfcba1a5093715ee44eeaedf
Simon Green <elyimmy96@gmail.com>
2020-09-02 10:24:32 -0500

    Produce Consume Mult v1.5

--- prodconsmult.c
+++ prodconsmult.c
@@ -2,7 +2,7 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /*
 This program provides a possible solution for producer-consumer problem using mutex and semaphore.
 I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these values.
