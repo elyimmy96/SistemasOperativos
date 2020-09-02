b405c15dcdcedb54d810181e22dcb12f4b290a88
Simon Green <elyimmy96@gmail.com>
2020-09-02 10:17:56 -0500

    Produce Consume Mult v1.1

--- prodconsmult.c
+++ prodconsmult.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
   sem_init(&vacio, 0, 5);
   sem_init(&mutex, 0, 1);
 
-  int [5] = {1, 2, 3, 4, 5};
+  int a[5] = {1, 2, 3, 4, 5};
 
   for (int i = 0; i < 5; i++) {
     pthread_create(&threadProduce[i], &attr, consume, &a[i]);
